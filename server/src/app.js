0295c3b886de807aacbcccae70e654b3583aced2
Shantanu <shanmishra2114@gmail.com>
2025-08-28 00:15:26 +0530

    cors updated

--- server/src/app.js
+++ server/src/app.js
@@ -16,7 +16,11 @@ dotenv.config();
 const app = express();
 
 const origin = process.env.FRONTEND_ORIGIN || 'http://localhost:8080';
-app.use(cors({ origin }));
+app.use(cors({
+  origin,
+  credentials: true
+}));
+
 app.use(helmet());
 app.use(express.json({ limit: '2mb' }));
 app.use(express.urlencoded({ extended: true }));
